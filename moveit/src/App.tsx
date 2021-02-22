a2af5cd4d0212ab9e1dc4d50c616860abee412b6
rvlyra <rubensvlyra@gmail.com>
2021-02-22 16:20:11 -0300

    Código fonte da aplicação - Aula#1

---
+++ moveit/src/App.tsx
@@ -0,0 +1,15 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css'
+
+function App() {
+  return (
+    //<h1>Olá, NLW!</h1>
+
+    <div className="container">
+      <ExperienceBar />
+    </div >
+
+  );
+}
+
+export default App;
---
+++ moveit/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <App />,  // JSX - javascript + xml
+
+  // Para criar components trabalhando com erros e exceptions
+  // <React.StrictMode> code/component </React.StrictMode>,
+
+  document.getElementById('root')
+);
